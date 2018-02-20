5e7bc9e986f469e6482e02693147f4bf557e0c82
JKapple <jkapple@students.mchenry.edu>
2018-02-19 22:35:38 -0600

    Add files via upload

---
+++ 5.4 Calories by Macronutrient.py
@@ -0,0 +1,7 @@
+def Calories():
+    input('Enter the calories from the following: Press enter when ready:')
+    fat = input * 9('calories from fat:')
+    carbs = input * 4('calories from carbs:')
+    protein = input * 4('Calories from protein:')
+    Calories == fat + carbs + protein
+    print(Calories)
---
+++ 5.6 Grade Calculation.py
@@ -0,0 +1,9 @@
+input('Please enter 5 test scores: press enter when ready')
+first_test_score = input('First test score:')
+second_test_score = input('Second test score:')
+third_test_score = input('Third test score:')
+fourth_test_score = input('Fourth test score:')
+fifth_test_score = input('Fifth test score:')
+total = first_test_score + second_test_score + third_test_score + fourth_test_score + first_test_score
+calc_average = total/5
+print (calc_average)
